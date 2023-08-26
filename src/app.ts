8bafb2e3e35991a4a884fd2121fbf703e2a8753e
alexzender45 <alexzendersamuel33@gmail.com>
2023-08-26 05:34:25 +0100

    completed

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
-import config from "config";
 import log from "./utils/logger";
 import routes from "./routes";
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = config.get<number>("PORT");
 const app = express();
 
 app.use(express.json());
 
-app.listen(port, async () => {
+app.listen(process.env.PORT, async () => {
   log.info("Listening on port 3000");
   routes(app);
 });
